salt-formulas/salt-formula-heka

commit 74ad71d410c67c634f31b81e182862f1ebbf50a8

Update collectd decoder to parse ntpd metrics

heka/files/lua/decoders/collectd.lua
@@ -216,6 +216,14 @@ function process_message ()
                 else
                     msg['Fields']['name'] = metric_name
                 end
+            elseif metric_source == 'ntpd' then
+                if sample['type_instance'] == 'error' or sample['type_instance'] == 'loop' then
+                    msg['Fields']['name'] = 'ntp' .. sep .. sample['type'] .. sep .. sample['type_instance']
+                else
+                    msg['Fields']['name'] = 'ntp' .. sep .. sample['type'] .. sep .. 'peer'
+                    msg['Fields']['server'] = sample['type_instance']
+                    table.insert(msg['Fields']['tag_fields'], 'server')
+                end
             elseif metric_source == 'check_openstack_api' then
                 -- For OpenStack API metrics, plugin_instance = <service name>
                 msg['Fields']['name'] = 'openstack_check_api'

commit 03a7d99c139d4e0c71064fb71adc28031fb0bcb9

Fix collectd Lua decoder for system metrics
The regression has been introduced by 74ad71d41.

heka/files/lua/decoders/collectd.lua
@@ -121,6 +121,10 @@ function process_message ()
             if sample['meta'] and sample['meta']['service_check'] then
                 msg['Fields']['name'] = sample['meta']['service_check'] .. sep .. 'check'
                 msg['Fields']['details'] = sample['meta']['failure']
+            elseif metric_source == 'memory' or metric_source == 'contextswitch' or
+                   metric_source == 'entropy' or metric_source == 'load' or
+                   metric_source == 'swap' or metric_source == 'uptime' then
+                msg['Fields']['name'] = metric_name
             elseif metric_source == 'df' then
                 local entity
                 if sample['type'] == 'df_inodes' then